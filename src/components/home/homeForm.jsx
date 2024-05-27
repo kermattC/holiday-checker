f030011e60def0ffc461d85855d575d2a290f39a
Matt <kermitbadaboom@gmail.com>
2024-05-27 16:58:58 -0400

    Implemented zeller's congruence and other date finding functions

--- src/components/home/homeForm.jsx
+++ src/components/home/homeForm.jsx
@@ -32,6 +32,12 @@ const Home = () => {
 
     useEffect(() => {
         fetchData()
+
+        const testYear = 2024;
+        const testMonth = 6;
+        const testDate = 1;
+        console.log(`There are ${getDaysInMonth(testYear, testMonth)} days in ${testMonth}`)
+        console.log(`${testDate}/${testMonth}/${testYear} is a ${zellersCongruence(testDate, testMonth, testYear)}`);
     }, [])
 
     useEffect(() => {
@@ -39,6 +45,91 @@ const Home = () => {
             console.log("Data updated: ", data);
         }
     }, [data]);
+
+    // functions for populating calendar 
+
+    // check for leap year
+    const isLeapYear = (year) => {
+        if ((year % 4 === 0 && year % 100 != 0) || (year % 400 === 0)) {
+            return True
+        }
+        return False
+    }
+
+    // get the number of days in the month
+    const getDaysInMonth = (year, month) => {
+        
+        // february case
+        if (month === 2){
+            return isLeapYear(year) ? 29 : 28
+        }
+
+        // months with 30 days
+        const smallMonths = [4, 6, 9, 11]
+        if (smallMonths.includes(month)) {
+            return 30
+        }
+
+        // default case is month with 31 days
+        return 31
+    }
+
+    // use zeller's congruence to find which day the 1st belongs to
+    const zellersCongruence = (day, month, year) => {
+
+        if (month == 1)
+        {
+            month = 13;
+            year--;
+        }
+        if (month == 2)
+        {
+            month = 14;
+            year--;
+        }
+        let q = day;
+        let m = month;
+        let k = year % 100;
+        let j = parseInt(year / 100, 10);
+        let h = q + parseInt(13 * (m + 1) / 5, 10) + k + parseInt(k / 4, 10) + parseInt(j / 4, 10) + 5 * j;
+        h = h % 7;
+        switch (h)
+        {
+            case 0 : 
+              return ('saturday')
+            //   break;
+                       
+            case 1 : 
+                return ('sunday')
+                // document.write("Sunday");
+                // break;
+                       
+            case 2 : 
+                return ('monday')
+                // document.write("Monday");
+                // break;
+                       
+            case 3 : 
+                return ('tuesday')
+                // document.write("Tuesday");
+                // break;
+                       
+            case 4 : 
+                return ('wednesday')
+                // document.write("Wednesday");
+                // break;
+                       
+            case 5 : 
+                return ('thursday')
+                // document.write("Thursday");
+                // break;
+                       
+            case 6 : 
+                return ('friday')
+                // document.write("Friday");
+                // break;
+        }
+    }
     
     return (
         <Table striped bordered hover>
